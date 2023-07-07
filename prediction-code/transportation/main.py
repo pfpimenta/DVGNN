0dea6a9173523912978537797da0275f96003e18
pfpimenta <pedrofolettopimenta@gmail.com>
2023-07-07 18:13:05 +0200

    main refactor

--- prediction-code/transportation/main.py
+++ prediction-code/transportation/main.py
@@ -17,15 +17,10 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
-import torch.optim as optim
+import torch.optim
 from dvgcn import DVGCN
-from lib.data_preparation import read_and_generate_dataset
-from lib.utils import compute_val_loss, evaluate, predict, scaled_Laplacian
-from torch.nn.modules.loss import _Loss as TorchLoss
-from torch.optim.optimizer import Optimizer
-from torch.utils.data import DataLoader, TensorDataset
-from tqdm import tqdm
-from utils import save_model_checkpoint, save_training_report
+from lib.utils import compute_val_loss, evaluate, predict
+from utils import evaluation, get_data_loaders, save_training_report, train
 
 # set torch seed to assure reprocibility
 torch.manual_seed(0)
@@ -99,270 +94,25 @@ use_mixhop = FLAGS.use_mixhop
 dynamic_graph = FLAGS.dynamic_graph
 
 
-# if dataset_name == "Tdrive":
-#     points_per_hour = 12
-#     num_of_features = 1
-#     merge = False
-#     num_for_predict = 12
-#     num_of_weeks = 2
-#     num_of_days = 1
-#     num_of_hours = 2
-# if dataset_name == "Los_loop":
-#     points_per_hour = 6
-#     num_of_features = 1
-#     merge = False
-#     num_for_predict = 12
-#     num_of_weeks = 2
-#     num_of_days = 1
-#     num_of_hours = 2
-# if dataset_name == "PEMS08":
-#     points_per_hour = 12
-#     num_of_features = 3
-#     merge = False
-#     num_for_predict = 12
-#     num_of_weeks = 2
-#     num_of_days = 1
-#     num_of_hours = 2
-
-
-model_name = "DVGCN_%s" % dataset_name
-
-wdecay = 0.000
-
-# set device (CUDA or cpu)
-print("Torch version: " + torch.__version__)
-print("Is CUDA available? " + str(torch.cuda.is_available()))
-if torch.cuda.is_available():
-    device = torch.device(FLAGS.device)
-else:
-    device = torch.device("cpu")
-# device = torch.device("cpu")  # DEBUG
-print(f"Using device {device}")
-# breakpoint() # TODO use GPU
-
-
-print("Model is %s" % (model_name))
-
-prediction_path = "DVGCN_prediction_%s" % dataset_name
-
-
-# def get_data_loaders(dataset_name: str, batch_size: int, dataset_params: Dict[str, Any]) -> Tuple(DataLoader, DataLoader, DataLoader, np.ndarray, torch.Tensor):
-# TODO move to another file
-def get_data_loaders(
-    dataset_name: str,
-    batch_size: int,
-    dataset_params: Dict[str, Any],
-    device: torch.device,
-):
-    # get data paths
-    data_folderpath = Path(__file__).parent.resolve() / "data/"
-    adj_matrix_csv_filepath = data_folderpath / (
-        "%s/%s_adj.csv" % (dataset_name, dataset_name)
-    )
-    graph_signal_matrix_filename = data_folderpath / (
-        "%s/%s.npz" % (dataset_name, dataset_name)
-    )
-    generated_adj_filepath = (
-        Path(__file__).parent.resolve()
-        / f"generated_adj/dynamic_{dataset_name}_adj.npy"
-    )
-    # read all data from graph signal matrix file
-    print("Reading data...")
-    # load adj matrix
-    adj = np.mat(pd.read_csv(adj_matrix_csv_filepath, header=None), dtype=float)
-    adjs = scaled_Laplacian(adj)
-    supports = (torch.tensor(adjs)).type(torch.float32).to(device)
-    # Input: train / valid  / test : length x 3 x NUM_POINT x 12
-    all_data = read_and_generate_dataset(
-        dataset_name,
-        graph_signal_matrix_filename,
-        generated_adj_filepath,
-        dataset_params["num_of_weeks"],
-        dataset_params["num_of_days"],
-        dataset_params["num_of_hours"],
-        dataset_params["num_for_predict"],
-        dataset_params["points_per_hour"],
-        dataset_params["merge"],
-    )
-    # test set ground truth
-    true_value = all_data["test"]["target"]
-    print(true_value.shape)
-
-    # training set data loader
-    train_loader = DataLoader(
-        TensorDataset(
-            torch.Tensor(all_data["train"]["week"]),
-            torch.Tensor(all_data["train"]["day"]),
-            torch.Tensor(all_data["train"]["recent"]),  # hour
-            torch.Tensor(all_data["train"]["target"]),
-            torch.Tensor(all_data["train"]["recent_adj"]),
-        ),
-        batch_size=batch_size,
-        shuffle=True,
-    )
-
-    # validation set data loader
-    val_loader = DataLoader(
-        TensorDataset(
-            torch.Tensor(all_data["val"]["week"]),
-            torch.Tensor(all_data["val"]["day"]),
-            torch.Tensor(all_data["val"]["recent"]),  # hour
-            torch.Tensor(all_data["val"]["target"]),
-            torch.Tensor(all_data["val"]["recent_adj"]),
-        ),
-        batch_size=batch_size,
-        shuffle=False,
-    )
-
-    # testing set data loader
-    test_loader = DataLoader(
-        TensorDataset(
-            torch.Tensor(all_data["test"]["week"]),
-            torch.Tensor(all_data["test"]["day"]),
-            torch.Tensor(all_data["test"]["recent"]),  # hour
-            torch.Tensor(all_data["test"]["target"]),
-            torch.Tensor(all_data["test"]["recent_adj"]),
-        ),
-        batch_size=batch_size,
-        shuffle=False,
-    )
-    return train_loader, val_loader, test_loader, true_value, supports
-
-
-# TODO move to another file
-def evaluation(
-    model: torch.nn.Module,
-    test_loader: DataLoader,
-    true_value: np.ndarray,
-    supports: torch.Tensor,
-    device: torch.device,
-    epoch: int,
-):
-    # Evaluate on the test data
-    start_time_test = time()
-    test_rmse, test_mae, test_mape = evaluate(
-        model, test_loader, true_value, supports, device, epoch
-    )
-    end_time_test = time()
-    test_time = np.mean(end_time_test - start_time_test)
-    test_stats = {
-        "test_time": test_time,
-        "rmse": test_rmse,
-        "mae": test_mae,
-        "mape": test_mape,
-    }
-    # print("Test time: %.2f" % test_time)
-    return test_stats
-
-
-# TODO move to another file
-def train(
-    model: torch.nn.Module,
-    train_loader: DataLoader,
-    val_loader: DataLoader,
-    supports: torch.Tensor,
-    device: torch.device,
-    loss_function: TorchLoss,
-    optimizer: Optimizer,
-    epochs: int,
-):
-    clip = 5
-    val_loss_list = []
-    train_loss_list = []
-    train_time = []
-    rmse = []
-    mae = []
-    mape = []
-    training_start_timestamp = str(datetime.now().strftime("%Y_%m_%d_%Hh%M"))
-    print(f"training_start_timestamp: {training_start_timestamp}\n\n")
-
-    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, decay)
-
-    save_model_checkpoint(
-        model=model, epoch=0, training_start_timestamp=training_start_timestamp
-    )
-    for epoch in range(1, epochs + 1):
-        train_l = []
-        start_time_train = time()
-        # for train_w, train_d, train_r, train_t, train_adj_r in train_loader:
-        for i, batch in enumerate(tqdm(train_loader, desc="Training", file=sys.stdout)):
-            train_w, train_d, train_r, train_t, train_adj_r = batch
-            train_w = train_w.to(device)
-            train_d = train_d.to(device)
-            train_r = train_r.to(device)
-            train_t = train_t.to(device)
-            train_adj_r = train_adj_r.to(device)
-
-            model.train()  # train pattern
-            optimizer.zero_grad()  # grad to 0
-
-            output, _, A = model(train_w, train_d, train_r, supports, train_adj_r)
-
-            loss = loss_function(output, train_t)
-            # backward p
-            loss.backward()
-            # torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
-
-            # update parameter
-            optimizer.step()
-
-            training_loss = loss.item()
-            train_l.append(training_loss)
-        scheduler.step()
-        end_time_train = time()
-        train_l = np.mean(train_l)
-        train_loss_list.append(train_l)
-        print(
-            "epoch step: %s, training loss: %.2f, time: %.2fs"
-            % (epoch, train_l, end_time_train - start_time_train)
-        )
-        train_time.append(end_time_train - start_time_train)
-
-        # compute validation loss
-        valid_loss = compute_val_loss(
-            model, val_loader, loss_function, supports, device, epoch
-        )
-        val_loss_list.append(valid_loss)
-
-        # evaluate the model on testing set
-        rmse1, mae1, mape1 = evaluate(
-            model, test_loader, true_value, supports, device, epoch
-        )
-
-        rmse1 = round(rmse1, 4)
-        mae1 = round(mae1, 4)
-        mape1 = round(mape1, 4)
-
-        rmse.append(rmse1)
-        mae.append(mae1)
-        mape.append(mape1)
-
-        # save model checkpoint
-        save_model_checkpoint(
-            model=model, epoch=epoch, training_start_timestamp=training_start_timestamp
-        )
-
-    print("Training finished")
-    print("Training time/epoch: %.2f secs/epoch" % np.mean(train_time))
-    print("The min rmse is : " + str(min(rmse)))
-    print("The min rmse epoch is : " + str(rmse.index(min(rmse))))
-    print("The min mae is : " + str(min(mae)))
-    print("The min mae epoch is : " + str(mae.index(min(mae))))
-    print("The min mape is : " + str(min(mape)))
-    print("The min mape epoch is : " + str(mape.index(min(mape))))
-
-    training_stats = {
-        "train_loss": train_loss_list,
-        "val_loss": val_loss_list,
-        "train_time": train_time,
-        "rmse": rmse,
-        "mae": mae,
-        "mape": mape,
-    }
-    return model, training_start_timestamp, training_stats
-
-
 if __name__ == "__main__":
+    model_name = "DVGCN_%s" % dataset_name
+
+    wdecay = 0.000
+
+    # set device (CUDA or cpu)
+    print("Torch version: " + torch.__version__)
+    print("Is CUDA available? " + str(torch.cuda.is_available()))
+    if torch.cuda.is_available():
+        device = torch.device(FLAGS.device)
+    else:
+        device = torch.device("cpu")
+    # device = torch.device("cpu")  # force to CPU
+    print(f"Using device {device}")
+
+    print("Model is %s" % (model_name))
+
+    prediction_path = "DVGCN_prediction_%s" % dataset_name
+
     # get dataset params
     data_folderpath = Path(__file__).parent.parent.parent.resolve() / "data/"
     dataset_params_filepath = (
@@ -405,7 +155,7 @@ if __name__ == "__main__":
         "K": 3,
         "Kt": 3,
         "use_mixhop": use_mixhop,
-        "adjacency_powers": [0, 1, 2, 3],
+        "adjacency_powers": [0, 1, 2],
         "device": str(device),
     }
     print(f"Using model params: {model_params}")
@@ -422,17 +172,22 @@ if __name__ == "__main__":
     evaluate(model, test_loader, true_value, supports, device, epoch=0)
 
     # Optimizer: Adam
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=wdecay)
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=learning_rate, weight_decay=wdecay
+    )
 
     # train model
     model, training_start_timestamp, training_stats = train(
         model=model,
         train_loader=train_loader,
         val_loader=val_loader,
+        test_loader=test_loader,
         supports=supports,
+        true_value=true_value,
         device=device,
         loss_function=loss_function,
         optimizer=optimizer,
+        decay=decay,
         epochs=epochs,
     )
 
--- prediction-code/transportation/utils.py
+++ prediction-code/transportation/utils.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
+
 import json
+import sys
 from datetime import datetime
 from pathlib import Path
+
+# from utils import save_model_checkpoint
+from time import time
 from typing import Any, Dict, List
 
 import numpy as np
+import pandas as pd
 import torch
+import torch.optim
+from lib.data_preparation import read_and_generate_dataset
+from lib.utils import compute_val_loss, evaluate, scaled_Laplacian
+from torch.nn.modules.loss import _Loss as TorchLoss
+from torch.optim.optimizer import Optimizer
+from torch.utils.data import DataLoader, TensorDataset
+from tqdm import tqdm
 
 
 def get_project_root_folderpath() -> str:
@@ -74,7 +87,6 @@ def save_model_checkpoint(
     # performance_dict: Dict[str, Any],
     # training_loss_list: np.ndarray,
 ) -> None:
-    """TODO description"""
     # saves model at training_results/{timestamp}/{epoch}/model.pt
     training_results_dir = get_training_results_dir(
         epoch=epoch, training_start_timestamp=training_start_timestamp
@@ -82,31 +94,220 @@ def save_model_checkpoint(
     trained_model_filepath = training_results_dir / "model.pt"
     torch.save(model.state_dict(), trained_model_filepath)
     print(f"Saved {trained_model_filepath}")
-    # TODO save other infos as well
-    # prepare training information JSON
-    # checkpoint_training_report = model.training_report
-    # checkpoint_training_report["epoch"] = epoch
-    # checkpoint_training_report["step"] = step
-    # checkpoint_training_report["checkpoint_time"] = datetime.now().strftime(
-    #     "%Y_%m_%d_%Hh%M"
-    # )
-    # save training information JSON
-    # report_json_filepath = checkpoint_dir / "training_report.json"
-    # save_dict_to_json(
-    #     dict=checkpoint_training_report, json_filepath=report_json_filepath
-    # )
-    # print(f"Saved {report_json_filepath}")
-    # save performance JSON
-    # performance_json_filepath = checkpoint_dir / "performance.json"
-    # save_dict_to_json(dict=performance_dict, json_filepath=performance_json_filepath)
-    # print(f"Saved {performance_json_filepath}")
-    # save evaluation overview
-    # eval_overview_filepath = checkpoint_dir / "eval_overview.md"
-    # with open(eval_overview_filepath, "w") as f:
-    #     f.write(evaluation_overview)
-    # print(f"Saved {eval_overview_filepath}")
-    # # save training loss list
-    # training_loss_list_filepath = checkpoint_dir / "training_loss_list.npy"
-    # with open(training_loss_list_filepath, "wb") as file:
-    #     np.save(file, training_loss_list)
-    # print(f"Saved {training_loss_list_filepath}")
+
+
+# TODO move to another file
+def get_data_loaders(
+    dataset_name: str,
+    batch_size: int,
+    dataset_params: Dict[str, Any],
+    device: torch.device,
+):
+    # get data paths
+    data_folderpath = Path(__file__).parent.resolve() / "data/"
+    adj_matrix_csv_filepath = data_folderpath / (
+        "%s/%s_adj.csv" % (dataset_name, dataset_name)
+    )
+    graph_signal_matrix_filename = data_folderpath / (
+        "%s/%s.npz" % (dataset_name, dataset_name)
+    )
+    generated_adj_filepath = (
+        Path(__file__).parent.resolve()
+        / f"generated_adj/dynamic_{dataset_name}_adj.npy"
+    )
+    # read all data from graph signal matrix file
+    print("Reading data...")
+    # load adj matrix
+    adj = np.mat(pd.read_csv(adj_matrix_csv_filepath, header=None), dtype=float)
+    adjs = scaled_Laplacian(adj)
+    supports = (torch.tensor(adjs)).type(torch.float32).to(device)
+    # Input: train / valid  / test : length x 3 x NUM_POINT x 12
+    all_data = read_and_generate_dataset(
+        dataset_name,
+        graph_signal_matrix_filename,
+        generated_adj_filepath,
+        dataset_params["num_of_weeks"],
+        dataset_params["num_of_days"],
+        dataset_params["num_of_hours"],
+        dataset_params["num_for_predict"],
+        dataset_params["points_per_hour"],
+        dataset_params["merge"],
+    )
+    # test set ground truth
+    true_value = all_data["test"]["target"]
+    print(true_value.shape)
+
+    # training set data loader
+    train_loader = DataLoader(
+        TensorDataset(
+            torch.Tensor(all_data["train"]["week"]),
+            torch.Tensor(all_data["train"]["day"]),
+            torch.Tensor(all_data["train"]["recent"]),  # hour
+            torch.Tensor(all_data["train"]["target"]),
+            torch.Tensor(all_data["train"]["recent_adj"]),
+        ),
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+    # validation set data loader
+    val_loader = DataLoader(
+        TensorDataset(
+            torch.Tensor(all_data["val"]["week"]),
+            torch.Tensor(all_data["val"]["day"]),
+            torch.Tensor(all_data["val"]["recent"]),  # hour
+            torch.Tensor(all_data["val"]["target"]),
+            torch.Tensor(all_data["val"]["recent_adj"]),
+        ),
+        batch_size=batch_size,
+        shuffle=False,
+    )
+
+    # testing set data loader
+    test_loader = DataLoader(
+        TensorDataset(
+            torch.Tensor(all_data["test"]["week"]),
+            torch.Tensor(all_data["test"]["day"]),
+            torch.Tensor(all_data["test"]["recent"]),  # hour
+            torch.Tensor(all_data["test"]["target"]),
+            torch.Tensor(all_data["test"]["recent_adj"]),
+        ),
+        batch_size=batch_size,
+        shuffle=False,
+    )
+    return train_loader, val_loader, test_loader, true_value, supports
+
+
+# TODO move to another file
+def evaluation(
+    model: torch.nn.Module,
+    test_loader: DataLoader,
+    true_value: np.ndarray,
+    supports: torch.Tensor,
+    device: torch.device,
+    epoch: int,
+):
+    # Evaluate on the test data
+    start_time_test = time()
+    test_rmse, test_mae, test_mape = evaluate(
+        model, test_loader, true_value, supports, device, epoch
+    )
+    end_time_test = time()
+    test_time = np.mean(end_time_test - start_time_test)
+    test_stats = {
+        "test_time": test_time,
+        "rmse": test_rmse,
+        "mae": test_mae,
+        "mape": test_mape,
+    }
+    # print("Test time: %.2f" % test_time)
+    return test_stats
+
+
+# TODO move to another file
+def train(
+    model: torch.nn.Module,
+    train_loader: DataLoader,
+    val_loader: DataLoader,
+    test_loader: DataLoader,
+    supports: torch.Tensor,
+    true_value: torch.Tensor,
+    device: torch.device,
+    loss_function: TorchLoss,
+    optimizer: Optimizer,
+    decay: float,
+    epochs: int,
+):
+    clip = 5
+    val_loss_list = []
+    train_loss_list = []
+    train_time = []
+    rmse = []
+    mae = []
+    mape = []
+    training_start_timestamp = str(datetime.now().strftime("%Y_%m_%d_%Hh%M"))
+    print(f"training_start_timestamp: {training_start_timestamp}\n\n")
+
+    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, decay)
+
+    save_model_checkpoint(
+        model=model, epoch=0, training_start_timestamp=training_start_timestamp
+    )
+    for epoch in range(1, epochs + 1):
+        train_l = []
+        start_time_train = time()
+        for i, batch in enumerate(tqdm(train_loader, desc="Training", file=sys.stdout)):
+            train_w, train_d, train_r, train_t, train_adj_r = batch
+            train_w = train_w.to(device)
+            train_d = train_d.to(device)
+            train_r = train_r.to(device)
+            train_t = train_t.to(device)
+            train_adj_r = train_adj_r.to(device)
+
+            model.train()  # train pattern
+            optimizer.zero_grad()  # grad to 0
+
+            output, _, A = model(train_w, train_d, train_r, supports, train_adj_r)
+
+            loss = loss_function(output, train_t)
+            # backward p
+            loss.backward()
+            # torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+
+            # update parameter
+            optimizer.step()
+
+            training_loss = loss.item()
+            train_l.append(training_loss)
+        scheduler.step()
+        end_time_train = time()
+        train_l = np.mean(train_l)
+        train_loss_list.append(train_l)
+        print(
+            "epoch step: %s, training loss: %.2f, time: %.2fs"
+            % (epoch, train_l, end_time_train - start_time_train)
+        )
+        train_time.append(end_time_train - start_time_train)
+
+        # compute validation loss
+        valid_loss = compute_val_loss(
+            model, val_loader, loss_function, supports, device, epoch
+        )
+        val_loss_list.append(valid_loss)
+
+        # evaluate the model on testing set
+        rmse1, mae1, mape1 = evaluate(
+            model, test_loader, true_value, supports, device, epoch
+        )
+
+        rmse1 = round(rmse1, 4)
+        mae1 = round(mae1, 4)
+        mape1 = round(mape1, 4)
+
+        rmse.append(rmse1)
+        mae.append(mae1)
+        mape.append(mape1)
+
+        # save model checkpoint
+        save_model_checkpoint(
+            model=model, epoch=epoch, training_start_timestamp=training_start_timestamp
+        )
+
+    print("Training finished")
+    print("Training time/epoch: %.2f secs/epoch" % np.mean(train_time))
+    print("The min rmse is : " + str(min(rmse)))
+    print("The min rmse epoch is : " + str(rmse.index(min(rmse))))
+    print("The min mae is : " + str(min(mae)))
+    print("The min mae epoch is : " + str(mae.index(min(mae))))
+    print("The min mape is : " + str(min(mape)))
+    print("The min mape epoch is : " + str(mape.index(min(mape))))
+
+    training_stats = {
+        "train_loss": train_loss_list,
+        "val_loss": val_loss_list,
+        "train_time": train_time,
+        "rmse": rmse,
+        "mae": mae,
+        "mape": mape,
+    }
+    return model, training_start_timestamp, training_stats
