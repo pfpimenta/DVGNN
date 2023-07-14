1a70f7e214ba66baf938696c6f1f1bb4dfa2ad5f
pfpimenta <pedrofolettopimenta@gmail.com>
2023-07-14 12:45:50 +0200

    static adj matrix refactor

--- prediction-code/transportation/lib/utils.py
+++ prediction-code/transportation/lib/utils.py
@@ -1,23 +1,25 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 # pylint: disable=no-member
 
 import csv
-import numpy as np
-from scipy.sparse.linalg import eigs
-
-from .metrics import mean_absolute_error, mean_squared_error, masked_mape_np
-import torch
 from time import time
+
+import numpy as np
 import scipy.sparse as sp
 import tensorflow as tf
+import torch
+from scipy.sparse.linalg import eigs
+from torch.nn.modules.loss import _Loss as TorchLoss
+from torch.utils.data import DataLoader
 
+from .metrics import masked_mape_np, mean_absolute_error, mean_squared_error
 
 
 def normalized_adj(adj):
     adj = sp.coo_matrix(adj)
     rowsum = np.array(adj.sum(1))
     d_inv_sqrt = np.power(rowsum, -0.5).flatten()
-    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.0
     d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
     normalized_adj = adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
     normalized_adj = normalized_adj.astype(np.float32)
@@ -33,7 +35,7 @@ def sparse_to_tuple(mx):
 
 def calculate_laplacian(adj, lambda_max=1):
     L = normalized_adj(adj + np.eye(adj.shape[0]))
-    L=L+L**2+L**3+L**4+L**5
+    L = L + L**2 + L**3 + L**4 + L**5
 
     L = sp.csr_matrix(L)
     L = L.astype(np.float32)
@@ -42,15 +44,22 @@ def calculate_laplacian(adj, lambda_max=1):
 
 def weight_variable_glorot(input_dim, output_dim, name=""):
     init_range = np.sqrt(6.0 / (input_dim + output_dim))
-    initial = tf.random_uniform([input_dim, output_dim], minval=-init_range,
-                                maxval=init_range, dtype=tf.float32)
+    initial = tf.random_uniform(
+        [input_dim, output_dim], minval=-init_range, maxval=init_range, dtype=tf.float32
+    )
 
     return tf.Variable(initial, name=name)
 
 
-def search_data(sequence_length, num_of_batches, label_start_idx,
-                num_for_predict, units, points_per_hour):
-    '''
+def search_data(
+    sequence_length,
+    num_of_batches,
+    label_start_idx,
+    num_for_predict,
+    units,
+    points_per_hour,
+):
+    """
     Parameters
     ----------
     sequence_length: int, length of all history data
@@ -69,7 +78,7 @@ def search_data(sequence_length, num_of_batches, label_start_idx,
     Returns
     ----------
     list[(start_idx, end_idx)]
-    '''
+    """
 
     if points_per_hour < 0:
         raise ValueError("points_per_hour should be greater than 0!")
@@ -89,12 +98,20 @@ def search_data(sequence_length, num_of_batches, label_start_idx,
     if len(x_idx) != num_of_batches:
         return None
 
-    return x_idx[::-1]#倒叙输出,符合时间的 顺序输出,这里不占用多少空间
+    return x_idx[::-1]  # 倒叙输出,符合时间的 顺序输出,这里不占用多少空间
 
 
-def get_sample_indices(data_sequence,adj, num_of_weeks, num_of_days, num_of_hours,
-                       label_start_idx, num_for_predict, points_per_hour=12):
-    '''
+def get_sample_indices(
+    data_sequence,
+    adj,
+    num_of_weeks,
+    num_of_days,
+    num_of_hours,
+    label_start_idx,
+    num_for_predict,
+    points_per_hour=12,
+):
+    """
     Parameters
     ----------
     data_sequence: np.ndarray
@@ -125,48 +142,62 @@ def get_sample_indices(data_sequence,adj, num_of_weeks, num_of_days, num_of_hour
 
     target: np.ndarray
             shape is (num_for_predict, num_of_vertices, num_of_features)
-    '''
+    """
     if data_sequence.shape[0] < 3000:
-        week_indices = search_data(data_sequence.shape[0], num_of_weeks,
-                                   label_start_idx, num_for_predict,
-                                   24*1, points_per_hour)
+        week_indices = search_data(
+            data_sequence.shape[0],
+            num_of_weeks,
+            label_start_idx,
+            num_for_predict,
+            24 * 1,
+            points_per_hour,
+        )
     else:
-        week_indices = search_data(data_sequence.shape[0], num_of_weeks,
-                                   label_start_idx, num_for_predict,
-                                   24*7, points_per_hour)
+        week_indices = search_data(
+            data_sequence.shape[0],
+            num_of_weeks,
+            label_start_idx,
+            num_for_predict,
+            24 * 7,
+            points_per_hour,
+        )
     if not week_indices:
         return None
 
-    day_indices = search_data(data_sequence.shape[0], num_of_days,
-                              label_start_idx, num_for_predict,
-                              24, points_per_hour)
+    day_indices = search_data(
+        data_sequence.shape[0],
+        num_of_days,
+        label_start_idx,
+        num_for_predict,
+        24,
+        points_per_hour,
+    )
     if not day_indices:
         return None
 
-    hour_indices = search_data(data_sequence.shape[0], num_of_hours,
-                               label_start_idx, num_for_predict,
-                               1, points_per_hour)
+    hour_indices = search_data(
+        data_sequence.shape[0],
+        num_of_hours,
+        label_start_idx,
+        num_for_predict,
+        1,
+        points_per_hour,
+    )
     if not hour_indices:
         return None
-    c = [data_sequence[i: j] for i, j in week_indices]
-    week_sample = np.concatenate([data_sequence[i: j]
-                                  for i, j in week_indices], axis=0)
-    day_sample = np.concatenate([data_sequence[i: j]
-                                 for i, j in day_indices], axis=0)
-    hour_sample = np.concatenate([data_sequence[i: j]
-                                  for i, j in hour_indices], axis=0)
-    target = data_sequence[label_start_idx: label_start_idx + num_for_predict]
+    c = [data_sequence[i:j] for i, j in week_indices]
+    week_sample = np.concatenate([data_sequence[i:j] for i, j in week_indices], axis=0)
+    day_sample = np.concatenate([data_sequence[i:j] for i, j in day_indices], axis=0)
+    hour_sample = np.concatenate([data_sequence[i:j] for i, j in hour_indices], axis=0)
+    target = data_sequence[label_start_idx : label_start_idx + num_for_predict]
 
-    hour_sample_adj = np.concatenate([adj[i: j]
-                                  for i, j in hour_indices], axis=0)
-
-
+    hour_sample_adj = np.concatenate([adj[i:j] for i, j in hour_indices], axis=0)
 
     return week_sample, day_sample, hour_sample, target, hour_sample_adj
 
 
 def get_adjacency_matrix(distance_df_filename, num_of_vertices):
-    '''
+    """
     Parameters
     ----------
     distance_df_filename: str, path of the csv file contains edges information
@@ -177,15 +208,14 @@ def get_adjacency_matrix(distance_df_filename, num_of_vertices):
     ----------
     A: np.ndarray, adjacency matrix
 
-    '''
+    """
 
-    with open(distance_df_filename, 'r') as f:
+    with open(distance_df_filename, "r") as f:
         reader = csv.reader(f)
         header = f.__next__()
         edges = [(int(i[0]), int(i[1])) for i in reader]
 
-    A = np.zeros((int(num_of_vertices), int(num_of_vertices)),
-                 dtype=np.float32)
+    A = np.zeros((int(num_of_vertices), int(num_of_vertices)), dtype=np.float32)
 
     for i, j in edges:
         A[i, j] = 1
@@ -194,7 +224,7 @@ def get_adjacency_matrix(distance_df_filename, num_of_vertices):
 
 
 def scaled_Laplacian(W):
-    '''
+    """
     compute \tilde{L}
 
     Parameters
@@ -205,21 +235,21 @@ def scaled_Laplacian(W):
     ----------
     scaled_Laplacian: np.ndarray, shape (N, N)
 
-    '''
+    """
 
     assert W.shape[0] == W.shape[1]
 
     D = np.diag(np.sum(W, axis=1))
 
     L = D - W
-    
-    lambda_max = eigs(L, k=1, which='LR')[0].real
-    
+
+    lambda_max = eigs(L, k=1, which="LR")[0].real
+
     return (2 * L) / lambda_max - np.identity(W.shape[0])
 
 
 def cheb_polynomial(L_tilde, K):
-    '''
+    """
     compute a list of chebyshev polynomials from T_0 to T_{K-1}
 
     Parameters
@@ -232,7 +262,7 @@ def cheb_polynomial(L_tilde, K):
     ----------
     cheb_polynomials: list[np.ndarray], length: K, from T_0 to T_{K-1}
 
-    '''
+    """
 
     N = L_tilde.shape[0]
 
@@ -240,13 +270,22 @@ def cheb_polynomial(L_tilde, K):
 
     for i in range(2, K):
         cheb_polynomials.append(
-            2 * L_tilde * cheb_polynomials[i - 1] - cheb_polynomials[i - 2])
+            2 * L_tilde * cheb_polynomials[i - 1] - cheb_polynomials[i - 2]
+        )
 
     return cheb_polynomials
 
 
-def compute_val_loss(net, val_loader, loss_function, supports, device, epoch):
-    '''
+def compute_val_loss(
+    net: torch.nn.Module,
+    val_loader: DataLoader,
+    loss_function: TorchLoss,
+    static_adj: torch.Tensor,
+    device: torch.device,
+    epoch: int,
+    use_dynamic_graph: bool,
+):
+    """
     compute mean loss on validation set
 
     Parameters
@@ -259,28 +298,42 @@ def compute_val_loss(net, val_loader, loss_function, supports, device, epoch):
 
     epoch: int, current epoch
 
-    '''
+    """
     net.eval()
     with torch.no_grad():
         tmp = []
         for index, (val_w, val_d, val_r, val_t, val_adj_r) in enumerate(val_loader):
-            val_w=val_w.to(device)
-            val_d=val_d.to(device)
-            val_r=val_r.to(device)
-            val_t=val_t.to(device)
+            val_w = val_w.to(device)
+            val_d = val_d.to(device)
+            val_r = val_r.to(device)
+            val_t = val_t.to(device)
             val_adj_r = val_adj_r.to(device)
-            output,_,_ = net(val_w, val_d, val_r, supports,val_adj_r)
+
+            # dynamic or static adjacency matrix
+            if use_dynamic_graph:
+                adj = val_adj_r.to(device)
+            else:
+                batch_size = val_adj_r.shape[0]
+                adj = static_adj.repeat(batch_size, 1, 1).to(device)
+
+            output, _, _ = net(val_w, val_d, val_r, static_adj, adj)
             l = loss_function(output, val_t)
             tmp.append(l.item())
-    
+
         validation_loss = sum(tmp) / len(tmp)
-    
-        print('epoch: %s, validation loss: %.2f' % (epoch, validation_loss))
+
+        print("epoch: %s, validation loss: %.2f" % (epoch, validation_loss))
         return validation_loss
 
 
-def predict(net, test_loader, supports, device):
-    '''
+def predict(
+    net: torch.nn.Module,
+    test_loader: DataLoader,
+    static_adj: torch.Tensor,
+    device: torch.device,
+    use_dynamic_graph: bool,
+):
+    """
     predict
 
     Parameters
@@ -294,47 +347,73 @@ def predict(net, test_loader, supports, device):
     prediction: np.ndarray,
                 shape is (num_of_samples, num_of_vertices, num_for_predict)
 
-    '''
+    """
     net.eval()
     with torch.no_grad():
         prediction = []
-        for index, (test_w, test_d, test_r, test_t, test_adj_r) in enumerate(test_loader):
-            test_w=test_w.to(device)
-            test_d=test_d.to(device)
-            test_r=test_r.to(device)
-            test_t=test_t.to(device)
+        for index, (test_w, test_d, test_r, test_t, test_adj_r) in enumerate(
+            test_loader
+        ):
+            test_w = test_w.to(device)
+            test_d = test_d.to(device)
+            test_r = test_r.to(device)
+            test_t = test_t.to(device)
             test_adj_r = test_adj_r.to(device)
-            output,_,_=net(test_w, test_d, test_r, supports,test_adj_r)
+
+            # dynamic or static adjacency matrix
+            if use_dynamic_graph:
+                adj = test_adj_r.to(device)
+            else:
+                batch_size = test_adj_r.shape[0]
+                adj = static_adj.repeat(batch_size, 1, 1).to(device)
+
+            output, _, _ = net(test_w, test_d, test_r, static_adj, adj)
             prediction.append(output.cpu().detach().numpy())
 
-
-
-
         start_adj = time()
-        #get first batch's spatial attention matrix    
-        for index, (test_w, test_d, test_r, test_t, test_adj_r) in enumerate(test_loader):
-            test_w=test_w.to(device)
-            test_d=test_d.to(device)
-            test_r=test_r.to(device)
-            test_t=test_t.to(device)
+        # get first batch's spatial attention matrix
+        for index, (test_w, test_d, test_r, test_t, test_adj_r) in enumerate(
+            test_loader
+        ):
+            test_w = test_w.to(device)
+            test_d = test_d.to(device)
+            test_r = test_r.to(device)
+            test_t = test_t.to(device)
             test_adj_r = test_adj_r.to(device)
-            _,spatial_at,temporal_at=net(test_w, test_d, test_r, supports,test_adj_r)
-            spatial_at=spatial_at.cpu().detach().numpy()
-            temporal_at=temporal_at.cpu().detach().numpy()
+
+            # TODO modularize this part (train, eval, val, etc)
+            # dynamic or static adjacency matrix
+            if use_dynamic_graph:
+                adj = test_adj_r.to(device)
+            else:
+                batch_size = test_adj_r.shape[0]
+                adj = static_adj.repeat(batch_size, 1, 1).to(device)
+
+            _, spatial_at, temporal_at = net(test_w, test_d, test_r, adj, adj)
+            spatial_at = spatial_at.cpu().detach().numpy()
+            temporal_at = temporal_at.cpu().detach().numpy()
             break
         print(spatial_at.shape)
-        calculate_laplacian(spatial_at[0,:,:])
+        calculate_laplacian(spatial_at[0, :, :])
 
-        end_adj= time()
+        end_adj = time()
         test_time = end_adj - start_adj
         print("create laplacian time: %.5f" % test_time)
 
         prediction = np.concatenate(prediction, 0)
-        return prediction,spatial_at,temporal_at
+        return prediction, spatial_at, temporal_at
 
 
-def evaluate(net, test_loader, true_value, supports, device, epoch):
-    '''
+def evaluate(
+    net: torch.nn.Module,
+    test_loader: DataLoader,
+    true_value: np.ndarray,
+    static_adj: torch.Tensor,
+    device: torch.device,
+    epoch: int,
+    use_dynamic_graph: bool = True,
+):
+    """
     compute MAE, RMSE, MAPE scores of the prediction
     for 3, 6, 12 points on testing set
 
@@ -351,24 +430,25 @@ def evaluate(net, test_loader, true_value, supports, device, epoch):
 
     epoch: int, current epoch
 
-    '''
+    """
     net.eval()
     with torch.no_grad():
-        prediction, _, _ = predict(net, test_loader, supports, device)
+        # dynamic or static adjacency matrix
+        prediction, _, _ = predict(
+            net, test_loader, static_adj, device, use_dynamic_graph
+        )
 
         for i in [12]:
-            print('current epoch: %s, predict %s points' % (epoch, i))
+            print("current epoch: %s, predict %s points" % (epoch, i))
 
-            mae = mean_absolute_error(true_value[:, :, 0:i],
-                                  prediction[:, :, 0:i])
-            rmse = mean_squared_error(true_value[:, :, 0:i],
-                                  prediction[:, :, 0:i]) ** 0.5
-            mape = masked_mape_np(true_value[:, :, 0:i],
-                              prediction[:, :, 0:i], 0)
-            var = 1 - (np.var(true_value[:, :, 0:i] - prediction[:, :, 0:i])) / np.var(true_value[:, :, 0:i])
-            print('MAE: %.2f' % (mae))
-            print('RMSE: %.2f' % (rmse))
-    return rmse,mae,mape
-
-
-        
+            mae = mean_absolute_error(true_value[:, :, 0:i], prediction[:, :, 0:i])
+            rmse = (
+                mean_squared_error(true_value[:, :, 0:i], prediction[:, :, 0:i]) ** 0.5
+            )
+            mape = masked_mape_np(true_value[:, :, 0:i], prediction[:, :, 0:i], 0)
+            var = 1 - (np.var(true_value[:, :, 0:i] - prediction[:, :, 0:i])) / np.var(
+                true_value[:, :, 0:i]
+            )
+            print("MAE: %.2f" % (mae))
+            print("RMSE: %.2f" % (rmse))
+    return rmse, mae, mape
